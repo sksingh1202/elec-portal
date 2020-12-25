59e13c6d40853384039d09c1f3a8284bd55c1191
Saket Kumar Singh <saketkumar1202@gmail.com>
2020-12-25 19:17:55 +0530

    Added Buttons, info image, built the layout responsive.

--- new_vote.js
+++ new_vote.js
@@ -1,51 +1,124 @@
-function changeLabel() {
-    document.getElementById("list_index").style.opacity = 0;
-    document.getElementById("list_index").textContent = "Vote for";
-    document.getElementById("list_index").style.opacity = 1;
-}
+let selected = false;
+let selected_id = null;
 
-// let list_indices = document.getElementsByClassName("list_index");
+ids = ["A", "B", "C", "D"];
 
-// for (let index = 0; index < list_indices.length; index++) {
-//     const element = list_indices[index];
-//     let original = null;
-//     element.addEventListener("mouseover", function () {
-//         original = element.textContent;
-//         element.textContent = "Vote for";
-//     });
-//     element.addEventListener("mouseout",  function (original){
-//         element.textContent = original;
-//     });
-// }
+$(document).ready(function () {
+  console.log("ready!");
 
-// let orignal = "A"
-// document.getElementById("list_index").addEventListener("mouseover", function => () {
-//     original = document.getElementById("list_index").textContent;
-//     document.getElementById("list_index").textContent = "Vote for";
-// })
-// .
-// document.getElementById("list_index").addEventListener("mouseout",  function => (original){
-//     document.getElementById("list_index").textContent = original;
-// })
-
-// $('.candidiate').hover(
-//     $('.vote_text').removeClass('hidden');
-//     $('index').addClass('hidden')
-// )
-
-
-// $('.index').hover(
-//     function(){ $(this).addClass('hidden')},
-//     function(){ $(this).removeClass('hidden') });
-
-$(document).ready(function() {
-    console.log("ready!");
-    $('#candidiate').hover(function(){ 
-        $('#index').addClass('hidden');
-        $('#vote_text').removeClass('hidden');  
-    },     
-    function(){    
-        $('#index').removeClass('hidden'); 
-        $('#vote_text').addClass('hidden');  
+  ids.forEach((id) => {
+    $("#" + id).hover(
+      function () {
+        $("#index_" + id).addClass("hidden");
+        $("#text_" + id).removeClass("hidden");
+      },
+      function () {
+        $("#index_" + id).removeClass("hidden");
+        $("#text_" + id).addClass("hidden");
+      }
+    );
+    $("#" + id).click(function () {
+      $(".voter_list").css("opacity", 1);
+      $(".bg-custom").css("background", "rgb(255, 210, 177)");
+      $(".text-custom").css("color", "#E7690F");
+      if (
+        (selected == false) &
+        ((selected_id != input_id) | (input_id == null))
+      ) {
+        $(this).delay(10).fadeOut(70).fadeIn(70).fadeOut(50).fadeIn(50);
+        $(this).addClass(" bg-gray-200");
+        $("#action_box_container_" + id).addClass(" bg-black text-white");
+        $("#action_box_container_" + id).html("<span>Voted</span>");
+        selected = true;
+        selected_id = id;
+      } else if (id == selected_id) {
+        $("#" + selected_id).removeClass(" bg-gray-200");
+        $("#action_box_container_" + selected_id).removeClass(
+          " bg-black text-white"
+        );
+        $("#action_box_container_" + selected_id).html(
+          `<span id="text_${selected_id}" class="hidden" >Vote for</span><span id="index_${selected_id}">${selected_id}</span>`
+        );
+        selected = false;
+        selected_id = null;
+      } else {
+        $("#" + selected_id).removeClass(" bg-gray-200");
+        $("#action_box_container_" + selected_id).removeClass(
+          " bg-black text-white"
+        );
+        $("#action_box_container_" + selected_id).html(
+          `<span id="text_${selected_id}" class="hidden" >Vote for</span><span id="index_${selected_id}">${selected_id}</span>`
+        );
+        $(this).delay(10).fadeOut(70).fadeIn(70).fadeOut(50).fadeIn(50);
+        $(this).addClass(" bg-gray-200");
+        $("#action_box_container_" + id).addClass(" bg-black text-white");
+        $("#action_box_container_" + id).html("<span>Voted</span>");
+        selected = true;
+        selected_id = id;
+      }
+      console.log(selected_id);
     });
+  });
+
+  $("#nota").click(function () {
+    console.log("Nota ran!");
+    if (selected_id) {
+      $("#" + selected_id).removeClass(" bg-gray-200");
+      $("#action_box_container_" + selected_id).removeClass(
+        " bg-black text-white"
+      );
+      $("#action_box_container_" + selected_id).html(
+        `<span id="text_${selected_id}" class="hidden" >Vote for</span><span id="index_${selected_id}">${selected_id}</span>`
+      );
+      selected = false;
+      selected_id = null;
+    }
+    $(".voter_list").css("opacity", 0.5);
+    $(".bg-custom").css("background", "#E7690F");
+    $(".text-custom").css("color", "rgb(236, 232, 229)");
+  });
 });
+
+let input_id = null;
+document.onkeyup = function (e) {
+  if (e.which == 65) {
+    input_id = "A";
+  } else if (e.which == 66) {
+    input_id = "B";
+  } else if (e.which == 67) {
+    input_id = "C";
+  } else if (e.which == 68) {
+    input_id = "D";
+  } else {
+    input_id = null;
+  }
+
+  if (input_id) {
+    $(".voter_list").css("opacity", 1);
+    $(".bg-custom").css("background", "rgb(255, 210, 177)");
+    $(".text-custom").css("color", "#E7690F");
+
+    $("#" + selected_id).removeClass(" bg-gray-200");
+    $("#action_box_container_" + selected_id).removeClass(
+      " bg-black text-white"
+    );
+    $("#action_box_container_" + selected_id).html(
+      `<span id="text_${selected_id}" class="hidden" >Vote for</span><span id="index_${selected_id}">${selected_id}</span>`
+    );
+    $("#" + input_id)
+      .delay(10)
+      .fadeOut(70)
+      .fadeIn(70)
+      .fadeOut(50)
+      .fadeIn(50);
+    $("#"+input_id).addClass(" bg-gray-200");
+    $("#action_box_container_" + input_id).addClass(" bg-black text-white");
+    $("#action_box_container_" + input_id).html("<span>Voted</span>");
+    selected_id = input_id;
+    selected = true;
+    console.log(selected_id);
+  } else {
+    console.log("false key");
+  }
+};
+
